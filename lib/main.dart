4f88ee9d6ce4f9e31372d5200b334633fe084db5
Sridhar2412 <sridharsrinivasan24@gmail.com>
2023-06-10 15:32:34 +0530

    clip path

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:notification/presentation/home/home.dart';
 
-void main() {
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+
+  // final fcmToken = await FirebaseMessaging.instance.getToken();
+  // print("Registration Token:");
+  // print(fcmToken);
   runApp(const ProviderScope(child: MyAppPage()));
 }
--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -1,8 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:notification/core/services/notification_services.dart';
-import 'package:notification/presentation/home/providers/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 @RoutePage()
@@ -23,6 +21,84 @@ class MyAppPage extends StatelessWidget {
       ),
       home: const MyHomePage(title: 'Training'),
     );
+  }
+}
+
+class Clipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height);
+
+    var firstStart = Offset(size.width / 5, size.height);
+
+// third point of quadratic bezier curve
+    var firstEnd = Offset(size.width / 2.25, size.height - 50.0);
+
+// fourth point of quadratic bezier curve
+    path.quadraticBezierTo(
+        firstStart.dx, firstStart.dy, firstEnd.dx, firstEnd.dy);
+
+    var secondStart =
+        Offset(size.width - (size.width / 3.24), size.height - 105);
+
+// third point of quadratic bezier curve
+    var secondEnd = Offset(size.width, size.height - 10);
+
+// fourth point of quadratic bezier curve
+    path.quadraticBezierTo(
+        secondStart.dx, secondStart.dy, secondEnd.dx, secondEnd.dy);
+    path.lineTo(size.width, 0);
+    path.close();
+    // path.lineTo(0, size.height);
+    // path.lineTo(size.width, size.height);
+    // path.quadraticBezierTo(0, size.height, 0, 0);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
+
+class WaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height);
+
+    var firstStart = Offset(size.width / 5, size.height);
+
+// third point of quadratic bezier curve
+    var firstEnd = Offset(size.width / 2.25, size.height - 50.0);
+
+// fourth point of quadratic bezier curve
+    path.quadraticBezierTo(
+        firstStart.dx, firstStart.dy, firstEnd.dx, firstEnd.dy);
+
+    var secondStart =
+        Offset(size.width - (size.width / 3.24), size.height - 105);
+
+// third point of quadratic bezier curve
+    var secondEnd = Offset(size.width, size.height - 10);
+
+// fourth point of quadratic bezier curve
+    path.quadraticBezierTo(
+        secondStart.dx, secondStart.dy, secondEnd.dx, secondEnd.dy);
+    path.lineTo(size.width, 0);
+    path.close();
+    // path.lineTo(0, size.height);
+    // path.lineTo(size.width, size.height);
+    // path.quadraticBezierTo(0, size.height, 0, 0);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
   }
 }
 
@@ -65,54 +141,76 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Consumer(
-        builder: (context, ref, child) {
-          final newVal = ref.watch(nameProvider);
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextFormField(
-                  controller: nameController,
-                  keyboardType: TextInputType.text,
-                  onChanged: (value) {},
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    hintText: "Name",
-                  ),
-                ),
-                // ElevatedButton(
-                //     onPressed: () {
-                //       // print("hello");
-                //       notificationServices.sendNotification(
-                //           "News", "Sridhar here");
-                //     },
-                //     child: const Text("Send")),
-                // ElevatedButton(onPressed: () {}, child: const Text("Schedule")),
-                ElevatedButton(
-                    onPressed: () async {
-                      ref
-                          .read(nameProvider.notifier)
-                          .setName(nameController.text);
-                      final prefs = await SharedPreferences.getInstance();
-                      prefs.setString(KEYNAME, nameController.text);
-                    },
-                    child: const Text("Save")),
-                Text(newName),
-                // Text(
-                //   '$_counter',
-                //   style: Theme.of(context).textTheme.headlineMedium,
-                // ),
-              ],
+      body: Stack(
+        children: [
+          ClipPath(
+            clipper: Clipper(),
+            child: Container(
+              height: 200,
+              // width: 200,
+              color: Colors.amber,
             ),
-          );
-        },
+          ),
+          ClipPath(
+            clipper: Clipper(),
+            child: Container(
+              height: 180,
+              // width: 200,
+              color: Colors.deepOrange,
+            ),
+          ),
+        ],
       ),
+
+      //  Consumer(
+      //   builder: (context, ref, child) {
+      //     final newVal = ref.watch(nameProvider);
+      //     return Center(
+      //       child: Column(
+      //         mainAxisAlignment: MainAxisAlignment.center,
+      //         children: <Widget>[
+      //           TextFormField(
+      //             controller: nameController,
+      //             keyboardType: TextInputType.text,
+      //             onChanged: (value) {},
+      //             decoration: InputDecoration(
+      //               border: OutlineInputBorder(
+      //                   borderRadius: BorderRadius.circular(20)),
+      //               hintText: "Name",
+      //             ),
+      //           ),
+      //           // ElevatedButton(
+      //           //     onPressed: () {
+      //           //       // print("hello");
+      //           //       notificationServices.sendNotification(
+      //           //           "News", "Sridhar here");
+      //           //     },
+      //           //     child: const Text("Send")),
+      //           // ElevatedButton(onPressed: () {}, child: const Text("Schedule")),
+      //           ElevatedButton(
+      //               onPressed: () async {
+      //                 ref
+      //                     .read(nameProvider.notifier)
+      //                     .setName(nameController.text);
+      //                 final prefs = await SharedPreferences.getInstance();
+      //                 prefs.setString(KEYNAME, nameController.text);
+      //               },
+      //               child: const Text("Save")),
+      //           Text(newName),
+      //           // Text(
+      //           //   '$_counter',
+      //           //   style: Theme.of(context).textTheme.headlineMedium,
+      //           // ),
+      //         ],
+      //       ),
+      //     );
+      //   },
+      // ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
